0a06f2b5e4ab59d76794bdf736ac9a3faabb534b
asdros <szymonbraun@tlen.pl>
2018-12-23 20:51:56 +0100

    Work with the main menu selection interface

--- Simple Number Converter/Program.cs
+++ Simple Number Converter/Program.cs
@@ -1,104 +1,101 @@
 ﻿using System;
 
-public class Core
+namespace SimpleNumberConverter
 {
-
-    public static string ConvertTo(int value, int newBase)
+    public class Core
     {
-        string Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string result = "";
-        int remainder = 0;
 
-        if (value == 0)
-            return "0";
-
-        while (value > 0)
+        public static string ConvertTo(int value, int newBase)
         {
-            result = Chars[value % newBase] + result;
-            Console.WriteLine("\t\t" + value + "\t/\t" + newBase + " | " + result[remainder]);
-            value /= newBase;
-        }
-        return result;
-    }
+            string Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = "";
+            int remainder = 0;
 
-    public static int ConvertFrom(string number, int oldBase)
-    {
-        const int ASCII_LETTER_TO_VALUE = 55;
-        const int ASCII_DIGIT_TO_VALUE = 48;
-        int power = 0;
-        int sum = 0;
+            if (value == 0)
+                return "0";
 
-        foreach (char digit in number)
-        {
-            if (!(char.IsUpper(digit) || char.IsDigit(digit)))
-                throw new Exception($"znak {digit} jest po za zakresem!.");
-
-            int digitValue = char.IsDigit(digit) ? digit - ASCII_DIGIT_TO_VALUE : digit - ASCII_LETTER_TO_VALUE;
-
-            if (digitValue > (oldBase - 1))
-                throw new Exception($"Wartość cyfrowa {digitValue} jest większa od cyfry podstawy!");
-
-            sum = sum + ((int)Math.Pow(oldBase, power) * digitValue);
-
-            Console.WriteLine($"\t\t | {oldBase} ^ {power} * {digit} = {Math.Pow(oldBase, power) * digitValue} \t||{sum}");
-
-            power = power + 1;
-        }
-        Console.WriteLine("\t\n\nWynik pośredni, dziesiętnie: {0}\n\n", sum);
-
-        return sum;
-    }
-    public static void Input(int firstBase, int secondBase)
-    {
-        Console.Clear();
-        Console.WriteLine("\n\nPodaj liczbę w systemie " + firstBase + "-owym do przeliczenia: ");
-        string temp = Console.ReadLine();
-        int nvalue = Convert.ToInt32(temp);
-        if (firstBase == 10)
-        {
-            string dwa = ConvertTo(nvalue, secondBase);
-            Console.WriteLine("\n\n {0}\n\n", dwa);
-        }
-        else
-        {
-
-            int jeden = ConvertFrom(nvalue.ToString(), firstBase);
-            string dwa = ConvertTo(jeden, secondBase);
-            Console.WriteLine("\n\n {0}\n\n", dwa);
-        }
-
-    }
-    public static void Main()
-    {
-        bool status = false;
-        while (!status)
-        {
-            Console.WriteLine("\nPrzelicznik systemów liczbowych obejmujący system trójkowy i siódemkowy");
-            Console.WriteLine("\n\n\t1 - Z trójkowego na siódemkowy.\n\t2 - Z siódemkowego na trójkowy.\n\t3 - Z dowolnego systemu na dowolnie inny\n\t4 - Koniec programu.");
-            ConsoleKeyInfo button = Console.ReadKey();
-            switch (button.Key)
+            while (value > 0)
             {
-                case ConsoleKey.D1:
-                    Input(3, 7);
-                    break;
-                case ConsoleKey.D2:
-                    Input(7, 3);
-                    break;
-                case ConsoleKey.D3:
-                    Console.Clear();
-                    Console.WriteLine("\nPodaj początkowy system, a następnie finalny");
-                    int firstBase = Convert.ToInt32(Console.ReadLine());
-                    int secondBase = Convert.ToInt32(Console.ReadLine());
-                    Input(firstBase, secondBase);
-                    break;
-                case ConsoleKey.D4:
-                    status = true;
-                    break;
-                default:
-                    Console.WriteLine("\nNieprawidłowy wybór");
-                    break;
+                result = Chars[value % newBase] + result;
+                Console.WriteLine("\t\t" + value + "\t/\t" + newBase + " | " + result[remainder]);
+                value /= newBase;
+            }
+            return result;
+        }
+
+        public static int ConvertFrom(string number, int oldBase)
+        {
+            const int ASCII_LETTER_TO_VALUE = 55;
+            const int ASCII_DIGIT_TO_VALUE = 48;
+            int power = 0;
+            int sum = 0;
+
+            foreach (char digit in number)
+            {
+                if (!(char.IsUpper(digit) || char.IsDigit(digit)))
+                    throw new Exception($"znak {digit} jest po za zakresem!.");
+
+                int digitValue = char.IsDigit(digit) ? digit - ASCII_DIGIT_TO_VALUE : digit - ASCII_LETTER_TO_VALUE;
+
+                if (digitValue > (oldBase - 1))
+                    throw new Exception($"Wartość cyfrowa {digitValue} jest większa od cyfry podstawy!");
+
+                sum = sum + ((int)Math.Pow(oldBase, power) * digitValue);
+
+                Console.WriteLine($"\t\t | {oldBase} ^ {power} * {digit} = {Math.Pow(oldBase, power) * digitValue} \t||{sum}");
+
+                power = power + 1;
+            }
+            Console.WriteLine("\t\n\nWynik pośredni, dziesiętnie: {0}\n\n", sum);
+
+            return sum;
+        }
+        public static void Input(int firstBase, int secondBase)
+        {
+            Console.Clear();
+            Console.WriteLine("\n\nPodaj liczbę w systemie " + firstBase + "-owym do przeliczenia: ");
+            string temp = Console.ReadLine();
+            int nvalue = Convert.ToInt32(temp);
+            if (firstBase == 10)
+            {
+                string dwa = ConvertTo(nvalue, secondBase);
+                Console.WriteLine("\n\n {0}\n\n", dwa);
+            }
+            else
+            {
+
+                int jeden = ConvertFrom(nvalue.ToString(), firstBase);
+                string dwa = ConvertTo(jeden, secondBase);
+                Console.WriteLine("\n\n {0}\n\n", dwa);
+            }
+
+        }
+        public static void Main()
+        {
+            bool status = false;
+            while (!status)
+            {
+                Console.WriteLine("\nPrzelicznik systemów liczbowych o górnym zakresie "); //add the maximum range
+                Console.WriteLine("\n\t1 - Z \"dowolnego\" systemu na \"dowolnie\" inny\n\t2 - Koniec programu.");
+                ConsoleKeyInfo button = Console.ReadKey();
+                switch (button.Key)
+                {                   
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        Console.WriteLine("\nPodaj początkowy system, a następnie finalny");
+                        int firstBase = Convert.ToInt32(Console.ReadLine());
+                        int secondBase = Convert.ToInt32(Console.ReadLine());
+                        Input(firstBase, secondBase);
+                        break;
+                    case ConsoleKey.D2:
+                        status = true;
+                        break;
+                    default:
+                        Console.WriteLine("\nNieprawidłowy wybór");
+                        break;
+                }
             }
         }
     }
-}
 
+}
